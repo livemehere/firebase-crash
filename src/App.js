4a69fcf3531e8612c9e53a4a683b3bf3b7e32a3b
kongtaemin <rhdxoals@gmail.com>
2022-03-23 00:59:29 +0900

    firestore subcollection path로 읽기

--- src/App.js
+++ src/App.js
@@ -1,41 +1,41 @@
-import {db, firebaseManager} from "./service/firebase";
-import {useEffect, useState} from "react";
-
-
-
+import { db, firebaseManager } from "./service/firebase";
+import { useEffect, useState } from "react";
 
 function App() {
+  const handleCreateDoc = async () => {
+    // const createdDocId = await firebaseManager.createDoc('posts', {
+    //     love:'ha ji hyung'
+    // })
+    const createdDocId = await firebaseManager.createDocByPath(
+      "posts/lfj3n4JJg2QmvJLJlCgG/comments",
+      {
+        content: "두번쨰 덧글이야!",
+      }
+    );
+    console.log(createdDocId);
+  };
+  const handleReadDocs = async () => {
+    // const docs = await firebaseManager.readDocs('posts');
+    const docs = await firebaseManager.readDocsByPath(
+      "posts/lfj3n4JJg2QmvJLJlCgG/comments"
+    );
+    console.log(docs);
+  };
 
-    const handleCreateDoc = async ()=>{
-        // const createdDocId = await firebaseManager.createDoc('posts', {
-        //     love:'ha ji hyung'
-        // })
-        const createdDocId = await firebaseManager.createDocByPath('posts/lfj3n4JJg2QmvJLJlCgG/comments', {
-            content:'두번쨰 덧글이야!'
-        })
-        console.log(createdDocId);
-    }
-    const handleReadDocs = async ()=>{
-       // const docs = await firebaseManager.readDocs('posts');
-        const docs = await firebaseManager.readDocsByPath('posts/lfj3n4JJg2QmvJLJlCgG/comments');
-        console.log(docs);
-
-    }
-
-    const handleReadDoc = async ()=>{
-        // const doc = await firebaseManager.readDoc('posts', '9weQq1QhOBvzKdWySzWi');
-        const doc = await firebaseManager.readDocByPath('posts/lfj3n4JJg2QmvJLJlCgG/comments/mFknQBVo44w45gqJsDV4');
-        console.log(doc)
-
-
-    }
+  const handleReadDoc = async () => {
+    // const doc = await firebaseManager.readDoc('posts', '9weQq1QhOBvzKdWySzWi');
+    const doc = await firebaseManager.readDocByPath(
+      "posts/lfj3n4JJg2QmvJLJlCgG/comments/mFknQBVo44w45gqJsDV4"
+    );
+    console.log(doc);
+  };
 
   return (
     <div className="App">
       <h1>Firebase 파해치기</h1>
-        <button onClick={handleCreateDoc}>특정 컬렉션에 Doc 생성하기</button>
-        <button onClick={handleReadDocs}>특정 컬렉션에 Doc 모두 읽기</button>
-        <button onClick={handleReadDoc}>특정 컬렉션에 특정 Doc 읽기</button>
+      <button onClick={handleCreateDoc}>특정 컬렉션에 Doc 생성하기</button>
+      <button onClick={handleReadDocs}>특정 컬렉션에 Doc 모두 읽기</button>
+      <button onClick={handleReadDoc}>특정 컬렉션에 특정 Doc 읽기</button>
     </div>
   );
 }
--- src/service/firebase.js
+++ src/service/firebase.js
@@ -1,103 +1,107 @@
-import {initializeApp} from 'firebase/app';
-import {getFirestore,collection,addDoc,getDocs,doc,getDoc} from 'firebase/firestore';
-
-
+import { initializeApp } from "firebase/app";
+import {
+  getFirestore,
+  collection,
+  addDoc,
+  getDocs,
+  doc,
+  getDoc,
+} from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_KEY,
-    authDomain: process.env.REACT_APP_AUTHDOMAIN,
-    projectId: process.env.REACT_APP_PROJECTID,
-    storageBucket: process.env.REACT_APP_STORAGEBUCKET,
-    messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
-    appId:process.env.REACT_APP_APPID,
+  apiKey: process.env.REACT_APP_KEY,
+  authDomain: process.env.REACT_APP_AUTHDOMAIN,
+  projectId: process.env.REACT_APP_PROJECTID,
+  storageBucket: process.env.REACT_APP_STORAGEBUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
+  appId: process.env.REACT_APP_APPID,
 };
 
-class FirebaseManager{
-    constructor(config) {
-        this.firebaseApp = initializeApp(config);
-        this.db = getFirestore();
+class FirebaseManager {
+  constructor(config) {
+    this.firebaseApp = initializeApp(config);
+    this.db = getFirestore();
+  }
+
+  async createDoc(collectionName, data) {
+    if (typeof data !== "object") return;
+
+    let docRef;
+    try {
+      docRef = await addDoc(collection(this.db, collectionName), data);
+    } catch (e) {
+      console.error(e);
     }
+    return docRef.id;
+  }
 
-    async createDoc(collectionName,data){
-        if(typeof data !== "object") return;
+  async createDocByPath(path, data) {
+    if (typeof data !== "object") return;
 
-        let docRef;
-        try{
-            docRef = await addDoc(collection(this.db,collectionName),data);
-        }catch (e) {
-            console.error(e);
-        }
-        return docRef.id;
+    let docRef;
+    try {
+      docRef = await addDoc(collection(this.db, path), data);
+    } catch (e) {
+      console.error(e);
     }
+    return docRef.id;
+  }
 
-    async createDocByPath(path,data){
-        if(typeof data !== "object") return;
-
-        let docRef;
-        try{
-            docRef = await addDoc(collection(this.db,path),data);
-        }catch (e) {
-            console.error(e);
-        }
-        return docRef.id;
+  async readDocs(collectionName) {
+    const docs = [];
+    try {
+      const querySnapshot = await getDocs(collection(this.db, collectionName));
+      querySnapshot.forEach((doc) => {
+        docs.push(doc.data());
+      });
+    } catch (e) {
+      console.log(e);
     }
+    return docs;
+  }
 
-    async readDocs(collectionName){
-        const docs = [];
-        try{
-            const querySnapshot = await getDocs(collection(this.db,collectionName));
-            querySnapshot.forEach(doc=>{
-                docs.push(doc.data());
-            })
-        }catch(e){
-            console.log(e)
-        }
-        return docs;
+  async readDocsByPath(path) {
+    const docs = [];
+    try {
+      const querySnapshot = await getDocs(collection(this.db, path));
+      querySnapshot.forEach((doc) => {
+        docs.push(doc.data());
+      });
+    } catch (e) {
+      console.log(e);
     }
+    return docs;
+  }
+  /*
+   * doc을 사용하는 두 가지 방법
+   * 1. doc(this.db, collection, doc)
+   * 2. doc(this.db, 'collection/doc')
+   *
+   * 하나의 doc을 읽을땐 getDoc()을 사용한다.
+   * */
 
-    async readDocsByPath(path){
-        const docs = [];
-        try{
-            const querySnapshot = await getDocs(collection(this.db,path));
-            querySnapshot.forEach(doc=>{
-                docs.push(doc.data());
-            })
-        }catch(e){
-            console.log(e)
-        }
-        return docs;
+  async readDoc(collectionName, targetDocID) {
+    let targetDoc;
+    const targetDocRef = doc(this.db, collectionName, targetDocID); // or collection(db, 'users');
+    try {
+      targetDoc = await getDoc(targetDocRef);
+    } catch (e) {
+      console.log(e);
     }
-    /*
-    * doc을 사용하는 두 가지 방법
-    * 1. doc(this.db, collection, doc)
-    * 2. doc(this.db, 'collection/doc')
-    *
-    * 하나의 doc을 읽을땐 getDoc()을 사용한다.
-    * */
+    return targetDoc.data();
+  }
 
-    async readDoc(collectionName,targetDocID){
-        let targetDoc;
-        const targetDocRef = doc(this.db,collectionName,targetDocID); // or collection(db, 'users');
-        try{
-            targetDoc =  await getDoc(targetDocRef);
-        }catch (e) {
-            console.log(e)
-        }
-        return targetDoc.data();
+  // 이렇게 하면 하위 컬렉션을 계속 탐지해 나갈 수 있음
+  async readDocByPath(path) {
+    let targetDoc;
+    const targetDocRef = doc(this.db, path);
+    try {
+      targetDoc = await getDoc(targetDocRef);
+    } catch (e) {
+      console.log(e);
     }
-
-    // 이렇게 하면 하위 컬렉션을 계속 탐지해 나갈 수 있음
-    async readDocByPath(path){
-        let targetDoc;
-        const targetDocRef = doc(this.db,path);
-        try{
-            targetDoc =  await getDoc(targetDocRef);
-        }catch (e) {
-            console.log(e)
-        }
-        return targetDoc.data();
-    }
+    return targetDoc.data();
+  }
 }
 
 export const firebaseManager = new FirebaseManager(firebaseConfig);
-
